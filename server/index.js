3d834396711863bac088a0a70ba48ebce6b8a5ae
EclecticTaco <calvin5210@gmail.com>
2020-02-22 11:52:45 -0800

    implemented enqueue and dequeue methods

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
+const messages = require('./js/messageQueue')
 
-
-
-// const keypressHandler = require('./js/keypressHandler');
-// keypressHandler.initialize(message => console.log(`Message received: ${message}`));
+const keypressHandler = require('./js/keypressHandler');
+keypressHandler.initialize((message) => {messages.enqueue(message)});
 
 const httpHandler = require('./js/httpHandler');
 
--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -3,6 +3,7 @@ const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
 const keypressHandler = require('./keypressHandler');
+const messages = require('./messageQueue')
 // keypressHandler.initialize(message => console.log(`Message received: ${message}`));
 
 
@@ -23,9 +24,38 @@ keypressHandler.initialize(message => {
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
+  if (req.method === 'POST') {
+    var body = []
+    req.on('data', chunk => {
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      body = Buffer.concat(body).toString()
+      console.log(body)
+      fs.writeFile('image.jpg', body, err => {
+        if (err) {
+          console.log(err)
+        }
+      })
+      console.log('image saved')
+      res.end(body)
 
-  res.end(command.toString())
-  command = '';
+    })
+    console.log('POSTED')
+
+
+
+  } else if (req.method === 'GET') {
+    console.log('GET')
+
+    res.end(messages.dequeue())
+    // command = '';
+  }
+  // res.writeHead(200, headers);
+
+
+  // res.end(command.toString())
+  // command = '';
   next();
 }
   // fs.readFile('command.txt', function(error, fileContents) {
